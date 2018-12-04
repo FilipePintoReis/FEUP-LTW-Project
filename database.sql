5e0bac99e2a9e67287c5a58c9405523ac21bae8c
Unknown <lopes.pedroa@gmail.com>
2018-12-04 19:18:45 +0000

    Added new .sql file for DB

--- database.sql
+++ database.sql
@@ -1,31 +1,71 @@
-DROP TABLE Story;
-DROP TABLE Comment;
-DROP TABLE User;
 
+DROP TABLE IF EXISTS Story;
 CREATE TABLE Story (
-	ID  INT NOT NULL AUTO_INCREMENT,
-	title TEXT,
-	datePosted Date,
-	content TEXT,
-	imagem TEXT,
-	tags TEXT,
-	cathegories TEXT,
-	PRIMARY KEY (ID)
-)
+	id_story	INT PRIMARY KEY
+				UNIQUE
+				NOT NULL
+				AUTO_INCREMENT,
+	id_user		INT REFERENCES User(id_user)
+				NOT NULL,
+	id_channel 	INT REFERENCES Channel(id_channel)
+				NOT NULL,
+	title 		TEXT,
+	content 	TEXT,
+	date_posted DATE
+);
 
+DROP TABLE IF EXISTS Comment;
 CREATE TABLE Comment (
-	ID  INT NOT NULL AUTO_INCREMENT,
-	ID_story INT,
-	ID_user INT,
-	datePosted Date,
-	content TEXT,
-	FOREIGN KEY (ID_story) REFERENCES Story(ID_story),
-	FOREIGN KEY (ID_user) REFERENCES User(ID_user),
-	PRIMARY KEY (ID)
-)
+	id_comment  INT PRIMARY KEY
+				UNIQUE
+				NOT NULL
+				AUTO_INCREMENT,
+	id_story	INT REFERENCES Story(id_story)
+				NOT NULL,
+	id_user		INT REFERENCES User(id_user)
+				NOT NULL,
+	id_parent	INT REFERENCES Comment(id_comment),
+	content 	TEXT,
+	datePosted 	DATE
+);
 
+DROP TABLE IF EXISTS User;
 CREATE TABLE User (
-	ID  INT NOT NULL AUTO_INCREMENT,
-	UserName TEXT,
-	Password TEXT
-)
+	id_user		INT PRIMARY KEY
+				UNIQUE
+				NOT NULL
+				AUTO_INCREMENT,
+	username	TEXT,
+	password 	TEXT
+);
+
+DROP TABLE IF EXISTS Channel;
+CREATE TABLE Channel (
+	id_channel 	INT PRIMARY KEY
+				UNIQUE
+				NOT NULL
+				AUTO_INCREMENT,
+	name		TEXT,
+);
+
+DROP TABLE IF EXISTS StoryVote;
+CREATE TABLE StoryVote (
+	id_story	INT REFERENCES Story(id_story)
+				NOT NULL,
+	id_user		INT REFERENCES User(id_user)
+				NOT NULL,
+	value		INT,
+	CHECK 		(value in (-1, 0, 1)),
+	PRIMARY KEY (id_story, id_user)
+);
+
+DROP TABLE IF EXISTS CommentVote;
+CREATE TABLE CommentVote (
+	id_comment	INT REFERENCES Comment(id_comment)
+				NOT NULL,
+	id_user	INT REFERENCES User(id_user)
+				NOT NULL,
+	value		INT,
+	CHECK 		(value in (-1, 0, 1)),
+	PRIMARY KEY (id_comment, id_user)
+);
