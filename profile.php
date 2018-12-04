d03111217a35bbbd07e77c516e43cec0eaf5f3c7
FilipePintoReis <filipepintodosreis@gmail.com>
2018-12-04 20:50:25 +0000

    merge

--- profile.php
+++ profile.php
@@ -2,7 +2,6 @@
     include_once('templates/common.php');
     include_once('templates/profiles.php');
     include_once('templates/stories.php');
-
     draw_header(null);
     draw_profile(null);
     draw_stories_feed();
