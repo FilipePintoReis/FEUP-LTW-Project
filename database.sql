e9fb19224c0a2bc6b64890bae721f335a18d2dd1
FilipePintoReis <filipepintodosreis@gmail.com>
2018-12-07 23:56:40 +0000

    corrected database. INTEGER and not INT, AUTOINCREMENT not AUTOINCREMENT,  PRIMARY KEY and AUTOINCREMENT don't need NOT NULL and UNIQUE

--- database.sql
+++ database.sql
@@ -1,13 +1,11 @@
 
 DROP TABLE IF EXISTS Story;
 CREATE TABLE Story (
-	id_story	INT PRIMARY KEY
-				UNIQUE
-				NOT NULL
-				AUTO_INCREMENT,
-	id_user		INT REFERENCES User(id_user)
+	id_story	INTEGER PRIMARY KEY
+				AUTOINCREMENT,
+	id_user		INTEGER REFERENCES User(id_user)
 				NOT NULL,
-	id_channel 	INT REFERENCES Channel(id_channel)
+	id_channel 	INTEGER REFERENCES Channel(id_channel)
 				NOT NULL,
 	title 		TEXT,
 	content 	TEXT,
@@ -16,56 +14,50 @@ CREATE TABLE Story (
 
 DROP TABLE IF EXISTS Comment;
 CREATE TABLE Comment (
-	id_comment  INT PRIMARY KEY
-				UNIQUE
-				NOT NULL
-				AUTO_INCREMENT,
-	id_story	INT REFERENCES Story(id_story)
+	id_comment  INTEGER PRIMARY KEY
+				AUTOINCREMENT,
+	id_story	INTEGER REFERENCES Story(id_story)
 				NOT NULL,
-	id_user		INT REFERENCES User(id_user)
+	id_user		INTEGER REFERENCES User(id_user)
 				NOT NULL,
-	id_parent	INT REFERENCES Comment(id_comment),
+	id_parent	INTEGER REFERENCES Comment(id_comment),
 	content 	TEXT,
 	datePosted 	DATE
 );
 
 DROP TABLE IF EXISTS User;
 CREATE TABLE User (
-	id_user		INT PRIMARY KEY
-				UNIQUE
-				NOT NULL
-				AUTO_INCREMENT,
+	id_user		INTEGER PRIMARY KEY
+				AUTOINCREMENT,
 	username	TEXT,
 	password 	TEXT
 );
 
 DROP TABLE IF EXISTS Channel;
 CREATE TABLE Channel (
-	id_channel 	INT PRIMARY KEY
-				UNIQUE
-				NOT NULL
-				AUTO_INCREMENT,
-	name		TEXT,
+	id_channel 	INTEGER PRIMARY KEY
+				AUTOINCREMENT,
+	name		TEXT
 );
 
 DROP TABLE IF EXISTS StoryVote;
 CREATE TABLE StoryVote (
-	id_story	INT REFERENCES Story(id_story)
+	id_story	INTEGER REFERENCES Story(id_story)
 				NOT NULL,
-	id_user		INT REFERENCES User(id_user)
+	id_user		INTEGER REFERENCES User(id_user)
 				NOT NULL,
-	value		INT,
+	value		INTEGER,
 	CHECK 		(value in (-1, 0, 1)),
 	PRIMARY KEY (id_story, id_user)
 );
 
 DROP TABLE IF EXISTS CommentVote;
 CREATE TABLE CommentVote (
-	id_comment	INT REFERENCES Comment(id_comment)
+	id_comment	INTEGER REFERENCES Comment(id_comment)
 				NOT NULL,
-	id_user	INT REFERENCES User(id_user)
+	id_user	INTEGER REFERENCES User(id_user)
 				NOT NULL,
-	value		INT,
+	value		INTEGER,
 	CHECK 		(value in (-1, 0, 1)),
 	PRIMARY KEY (id_comment, id_user)
 );
