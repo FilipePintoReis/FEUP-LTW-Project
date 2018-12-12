a09779d36d2326ac24534d0be814567b1562561f
FilipePintoReis <filipepintodosreis@gmail.com>
2018-12-12 00:01:11 +0000

    added one comment

--- actions/action_create_post.php
+++ actions/action_create_post.php
@@ -6,8 +6,9 @@
 	$name = $all_info['name'];
     $path = "../post_images/$name"; 
 
-    if(move_uploaded_file($all_info['tmp_name'], $path))
-    	var_dump($name);
-    else
-    	var_dump($date);
- ?>
+    move_uploaded_file($all_info['tmp_name'], $path);
+
+    //insert_story($id_user, $id_channel, $title, $content, $date_posted, $url);	
+
+
+?>
