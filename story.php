3d8c1f7e6240f5a45422b9bd20203fa81b4e4379
Unknown <up201603811@fe.up.pt>
2018-12-13 13:29:47 +0000

    Show comments from story

--- story.php
+++ story.php
@@ -7,6 +7,9 @@
 
     $story = get_story_from_id($_GET['id']);
     $paragraphs = explode('\n', $story['content']);
+    $comments_result = get_all_comments_from_story($_GET['id']);
+    
+    
 
     include('templates/common/header.php');
     include('templates/tpl_story.php');
--- templates/tpl_story.php
+++ templates/tpl_story.php
@@ -50,8 +50,13 @@
             </span>
             <span id="comment"><a class="comments" href="story.php?id=<?=$story['id']?>#comments">Comment</a></span>
         </footer>
+        
     </article>
 </section>
 <section id="story_comments">
+    <?php foreach ($comments_result as $comment) {?>
+        <p><?= $comment['content'] ?></p>
+    <?php } ?>
+
 
 </section>
